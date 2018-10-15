2d0d33d5d6b6ca8f44783387278e389a5c2150c6
Pixelastic <tim@pixelastic.com>
2018-10-15 11:13:11 +0200

    Fix installation of webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,21 +1,10 @@
 /* eslint-disable import/no-commonjs */
 const merge = require('webpack-merge');
 const common = require('./webpack.config.common.js');
-const AutoDllPlugin = require('autodll-webpack-plugin');
-const package = require('./package.json');
 
 const webpackConfig = merge(common, {
   devtool: 'cheap-module-eval-source-map',
-  mode: 'production',
-  plugins: [
-    new AutoDllPlugin({
-      filename: '[name]_[hash].dll.js',
-      inherit: true,
-      entry: {
-        vendor: Object.keys(package.dependencies),
-      },
-    }),
-  ]
+  mode: 'development',
 });
 
 module.exports = webpackConfig;
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.config.common.js');
 
 const webpackConfig = merge(common, {
-  mode: 'development',
+  mode: 'production',
 });
 
 module.exports = webpackConfig;
