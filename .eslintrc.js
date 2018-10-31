e6abf243286a7bee6f83d1da809945a4a95c3ee1
Pixelastic <tim@pixelastic.com>
2018-10-31 11:29:43 +0100

    Use aberlaas for build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,4 @@
 module.exports = {
-  extends: ['algolia', 'algolia/jest'],
-  rules: {
-    'no-console': 0,
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-  },
+  extends: ['./node_modules/aberlaas/eslint.js'],
 };
+
