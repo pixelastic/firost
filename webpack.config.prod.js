b21fe411b8923e2cfa3d4bd6b98a0b7d995e81e0
Pixelastic <tim@pixelastic.com>
2018-10-31 09:49:37 +0100

    Build with Babel instead of Webpack

--- webpack.config.prod.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-commonjs */
-const merge = require('webpack-merge');
-const common = require('./webpack.config.common.js');
-
-const webpackConfig = merge(common, {
-  mode: 'production',
-});
-
-module.exports = webpackConfig;
