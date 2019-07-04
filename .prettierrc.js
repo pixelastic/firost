0b59c6071a47a71eb458f04dd0c8ed068473511c
Pixelastic <tim@pixelastic.com>
2019-07-04 15:49:28 +0200

    chore(deps): Update aberlaas

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-commonjs */
+const prettierConfig = require('aberlaas/build/configs/prettier.js');
+module.exports = {
+  ...prettierConfig,
+};
---
+++ .stylelintrc.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-commonjs */
+const stylelintConfig = require('aberlaas/build/configs/stylelint.js');
+module.exports = {
+  ...stylelintConfig,
+};
