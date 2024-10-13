653f3f983c91ff83dff0294ea8b9dc9fec486158
Tim Carry <tim@pixelastic.com>
2024-10-13 21:20:02 +0200

    fix(watch): Make sure we watch a directory that actually exists

--- docs/src/callstack.js
+++ docs/src/callstack.js
@@ -3,10 +3,11 @@ title: callstack
 ---
 
 <div class="lead">
-  Returns an array of the callstack, from where `callstack()` is called.
+  Returns info about the callstack, from where `callstack()` is called.
 </div>
 
 `callstack()`
+`callstack(n)`
 
 ## Examples
 
@@ -20,5 +21,11 @@ function myFunction() {
   //   "function": "myFunction"
   // }
 }
+
+// Or, targeting a specific depth in the callstack
+// 0 is the calling file,
+console.log(callstack(0));
+// 1 is the parent of the calling file
+console.log(callstack(1));
 
 ```
